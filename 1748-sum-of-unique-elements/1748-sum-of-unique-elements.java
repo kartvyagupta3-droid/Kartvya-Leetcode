10c461720f44e702d9986796c9a07a1272cb0256
Kartvyagupta19 <kartvyagupta3@gmail.com>
2026-07-06 22:04:41 +0530

    Time: 0 ms (100%), Space: 43 MB (54.77%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        int[] a = new int[101];
+        for(int i=0;i<nums.length;i++){
+            a[nums[i]]++;
+        }
+        int sum=0;
+        
+        for(int i=0;i<a.length;i++){
+            if(a[i]==1){
+                sum=sum+i;
+            }
+        }
+        return sum;
+    }
+}
